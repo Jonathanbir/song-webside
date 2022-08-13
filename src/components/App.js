0aedbc3e826ea4f47c22eb66c6488590f275b69d
Jonathanbir <jonathan79111@gmail.com>
2022-08-13 20:25:01 +0800

    commit home page

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,26 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import Music from "./Music";
+import { GlobalScrollProvider } from "../hooks/useGlobalScroll";
+import { GlobalMouseMoveProvider } from "../hooks/useGlobalMouseMove";
 import "../App.css";
+
+function Wrapper({ children }) {
+  return (
+    <GlobalScrollProvider>
+      <GlobalMouseMoveProvider>{children}</GlobalMouseMoveProvider>
+    </GlobalScrollProvider>
+  );
+}
 
 const App = () => {
   return (
-    <Routes>
-      <Route index element={<Home />} />
-      <Route path="music" element={<Music />} />
-    </Routes>
+    <Wrapper>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="music" element={<Music />} />
+      </Routes>
+    </Wrapper>
   );
 };
 
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,79 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeadphonesSimple } from "@fortawesome/free-solid-svg-icons";
+import { useGlobalMouseMove } from "../../hooks/useGlobalMouseMove";
 import "./index.css";
+
+const BackGround = () => {
+  const [mouseX, mouseY] = useGlobalMouseMove();
+
+  return (
+    <div
+      className="home-background"
+      style={{
+        transform: `translate(${10 * mouseX}px, ${-50 * mouseY}px) scale(1.2)`,
+      }}
+    />
+  );
+};
+
+const Eso = () => {
+  const [mouseX] = useGlobalMouseMove();
+
+  return (
+    <div
+      className="eso"
+      style={{
+        transform: `translate( ${30 * mouseX}px)`,
+      }}
+    />
+  );
+};
+
+const Bird = () => {
+  const [mouseX, mouseY] = useGlobalMouseMove();
+
+  return (
+    <div
+      className="bird"
+      style={{
+        transform: `translate(${10 * mouseX}px, ${-50 * mouseY}px) scale(1.2)`,
+      }}
+    />
+  );
+};
+
+const Tree = () => {
+  const [mouseX, mouseY] = useGlobalMouseMove();
+
+  return (
+    <div
+      className="tree"
+      style={{
+        transform: `translate(${10 * mouseX}px, ${
+          -50 * mouseY
+        }px) scale(1.2) rotate(30deg)`,
+      }}
+    />
+  );
+};
 
 const Home = () => {
   return (
     <div className="container">
-      <div className="eso" />
+      <BackGround />
+      <Eso />
+      <Bird />
+      <Tree />
       <Link className="nav-link" to="music">
-        <div className="music-btn">Listen</div>
+        <div className="music-btn">
+          <FontAwesomeIcon
+            className="font-icon-headphones"
+            icon={faHeadphonesSimple}
+          />
+          <p>Listen</p>
+        </div>
       </Link>
     </div>
   );
