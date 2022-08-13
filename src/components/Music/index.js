1bf7b1e561b65f1a897c7311b83f560198d9d401
Jonathanbir <jonathan79111@gmail.com>
2022-08-13 21:18:35 +0800

    commit music cd

--- src/components/Music/index.js
+++ src/components/Music/index.js
@@ -1,18 +1,12 @@
 import React from "react";
 import SongList from "../SongList";
 import SongDetail from "../SongDetail";
+import "./index.css";
 
 const Music = () => {
   return (
-    <div className="ui container grid">
-      <div className="ui row">
-        <div className="column eight wide">
-          <SongList />
-        </div>
-        <div className="column eight wide">
-          <SongDetail />
-        </div>
-      </div>
+    <div className="music-container">
+      <div className="cd"></div>
     </div>
   );
 };
