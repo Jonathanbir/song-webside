98873ad23810673398d6a4b16386ca24cc08469f
Jonathanbir <jonathan79111@gmail.com>
2022-08-13 13:22:21 +0800

    commit Link

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 
 const Home = () => {
   return (
     <div className="container">
       <div className="eso" />
+      <Link className="nav-link" to="music">
+        <div className="music-btn">Listen</div>
+      </Link>
     </div>
   );
 };
